4257751a80251ec7aba8fab5f48d8f30d56ddd05
linolica1 <linolica1@linolica-PC.home>
2016-05-27 01:51:10 -0300

    Plotado posição.

--- AppFrontController/web/js/lp3.js
+++ AppFrontController/web/js/lp3.js
@@ -1,68 +1,34 @@
-// carrega na variável userName o valor do campo escondido com id="userValue"
-
-//var userName = document.getElementById('userValue').value;
-var userName = "denis";
-//apresenta no console de execução do javascript o valor da variável userName
-console.log(userName);
-var container;
-var content;
-var closer;
-//define a url da chamada REST (GET)
-
-var urlString = 'http://localhost:8081/AppFrontController/LP3Rest/lp3/posicoes/';
-
-//concatena o nome do usuário à consulta REST
-urlString = urlString.concat(userName);
-//urlString2 = urlString2.concat(userName);
-
-var pointStyle = new ol.style.Style({
-    image: new ol.style.Icon(({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        opacity: 0.75,
-        src: 'dados/r2d2.png'
-    }))
-});
-//var pointStyle = new ol.style.Style({
-//    image: new ol.style.Icon(({
-//        anchor: [0, 0],
-//        anchorXUnits: 'fraction',
-//        anchorYUnits: 'fraction',
-//        opacity: 0.75,
-//        src: 'dados/r2d2.png'
-//    }))
-//});
-
 var meuMapa;
-function init() {
-    container = document.getElementById('popup');
-    content = document.getElementById('popup-content');
-    closer = document.getElementById('popup-closer');
-    var overlay = new ol.Overlay(({
-        element: container,
-        autoPan: true,
-        autoPanAnimation: {
-            duration: 250
+var userName = getCookie('usuario');//document.getElementById('userValue').value;
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
         }
-    }));
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-
+function init() {
     meuMapa = new ol.Map({
-        target: 'MeuMapa',
+        target: MeuMapa,
         renderer: 'canvas',
-        overlays: [overlay],
         view: new ol.View({
             projection: 'EPSG:900913',
-            center: [-5193252.61684, -2698365.38923],
-            zoom: 18
+            center: [0, 0],
+            zoom: 3
         })
     });
     var openStreetMapLayer = new ol.layer.Tile({
         source: new ol.source.OSM()
     });
-
-
     meuMapa.addLayer(openStreetMapLayer);
     var bingLayer = new ol.layer.Tile({
         source: new ol.source.BingMaps({
@@ -72,29 +38,42 @@ function init() {
     });
     bingLayer.setOpacity(.3);
     meuMapa.addLayer(bingLayer);
-    closer.onclick = function () {
-        overlay.setPosition(undefined);
-        closer.blur();
-        return false;
-    };
-
-    meuMapa.on('singleclick', function (evt) {
-        var coordinate = evt.coordinate;
-        var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(coordinate, 'EPSG:900913',
-                'EPSG:4326'));
-        content.innerHTML = '<p>Posição Atual:</p><code>' + hdms + '</code>';
-        overlay.setPosition(coordinate);
+    var pointStyle = new ol.style.Style({
+        image: new ol.style.Icon(({
+            anchor: [0, 0],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'fraction',
+            opacity: 0.75,
+            src: 'dados/r2d2.png'
+        }))
     });
+
+
+
+//apresenta no console de execução do javascript o valor da variável userName
+
+    console.log(userName);
+
+//define a url da chamada REST (GET)
+
+    var urlString = 'http://localhost:8081/AppFrontController/LP3Rest/lp3/posicoes/';
+
+//concatena o nome do usuário à consulta REST
+
+    urlString = urlString.concat(userName);
+
+//executa a chamada ao webservice REST, recebe a resposta em um vetor de objetos JSON chamado data
+
+//chama a função plotOnMap passando o vetor como parametro.
 
     $.ajax({
         url: urlString,
         data: {
-            format: 'json'
+            format: 'xml'
         },
         success: function (data) {
             console.log(data);
             plotOnMap(data);
-
         },
         error: function (e) {
             console.log(e.message);
@@ -102,59 +81,99 @@ function init() {
         type: 'GET'
     });
 
-}
-//Define um estilo para os pontos a serem plotados.
-
-//Indica o icone que desejamos utilizar para representar as posições
-
-
 
 //Function que recebe a lista de objetos e plota os pontos
 
-function plotOnMap(json) {
+    function plotOnMap(json) {
 
-    // cria arranjo de features
-    var features = new Array();
+        // cria arranjo de features
+        var features = new Array();
 
-    console.log(json.length);
-    var count = 0;
-    for (var i = 0; i < json.length; i++) {
+        console.log(json.length);
+        for (var i = 0; i < json.length; i++) {
 
-        // cria ponto a partir de coordenadas em graus
-        var point = [json[i].lon, json[i].lat];
+            // cria ponto a partir de coordenadas em graus
+            var point = [parseFloat(json[i].lon), parseFloat(json[i].lat)];
 
-        // converte para coordenadas de tela
-        var pos = ol.proj.fromLonLat(point);
+            // converte para coordenadas de tela
+            var pos = ol.proj.fromLonLat(point);
 
-        // cria feature
+            // cria feature
 
-        var feature = new ol.Feature(new ol.geom.Point(pos));
+            var feature = new ol.Feature(new ol.geom.Point(pos));
 
-        // define o estilo da feature
-        feature.setStyle(pointStyle);
+            // define o estilo da feature
+            feature.setStyle(pointStyle);
 
-        //adiciona feature a lista de features
+            //adiciona feature a lista de features
 
-        features[features.length] = feature;
+            features[features.length] = feature;
+        }
+
+        // cria um vetor de features para colocação em uma camada de visualização 
+        var source = new ol.source.Vector({
+            features: features
+        });
+
+        //cria uma camada com o vetor de features
+        featuresLayer = new ol.layer.Vector({
+            source: source,
+            style: pointStyle
+        });
+
+        //adiciona a camada ao mapa
+
+        meuMapa.addLayer(featuresLayer);
+        var element = document.getElementById('popup');
+        var popup = new ol.Overlay({
+            element: element,
+            positioning: 'bottom-center',
+            stopEvent: false
+        });
+        meuMapa.addOverlay(popup);
+        meuMapa.on('click', function (evt) {
+            var feature = meuMapa.forEachFeatureAtPixel(evt.pixel,
+                    function (feature, layer) {
+                        return feature;
+                    });
+            if (feature) {
+                popup.setPosition(evt.coordinate);
+                var xmlString;
+                var urlString = 'http://localhost:8081/AppFrontController/LP3Rest/lp3/posicoesXML/';
+                console.log(urlString);
+                urlString = urlString.concat(userName);
+                console.log(urlString);
+                $.ajax({
+                    url: urlString,
+                    data: {
+                        format: 'xml'
+                    },
+                    success: function (data) {
+                        xmlString = (new XMLSerializer()).serializeToString(data);
+                        console.log(xmlString);
+                        $(element).popover({
+                            'placement': 'top',
+                            'html': true,
+                            'content': '<p>' + xmlString + '</p>'
+                        });
+                        $(element).popover('show');
+                    },
+                    error: function (e) {
+                        console.log(e.message);
+                    },
+                    type: 'GET'
+                });
+            } else {
+                $(element).popover('destroy');
+            }
+        });
+        meuMapa.on('pointermove', function (e) {
+            if (e.dragging) {
+                $(element).popover('destroy');
+                return;
+            }
+            var pixel = meuMapa.getEventPixel(e.originalEvent);
+            var hit = meuMapa.hasFeatureAtPixel(pixel);
+        });
     }
-
-    // cria um vetor de features para colocação em uma camada de visualização 
-    var source = new ol.source.Vector({
-        features: features
-    });
-
-    //cria uma camada com o vetor de features
-    featuresLayer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-            projection: 'EPSG:4326',
-            format: new ol.format.GPX(),
-            url: 'dados/posicao_proc.gpx'
-        }),
-        style: pointStyle
-    });
-
-    //adiciona a camada ao mapa
-    meuMapa.addLayer(featuresLayer);
-
-
 }
